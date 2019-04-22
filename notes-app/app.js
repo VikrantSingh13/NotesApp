2ecb12641f6bd05f29783f6185fd266b154791c0
vikrantsingh13 <vikrantsinghvm@gmail.com>
2019-04-22 19:34:41 -0400

    fixed gitignore and ignored all .txt files

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
-// fs.writeFileSync("notes.txt", 
-// "All notes go here!");
+fs.writeFileSync("notes.txt", 
+"All notes go here!");
 
-fs.appendFileSync("notes.txt", 
-"\n We will keep adding more notes to this file!");
+// fs.appendFileSync("notes.txt", 
+// "\n We will keep adding more notes to this file!");
 
